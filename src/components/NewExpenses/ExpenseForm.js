97298050ee20a0e9bb851f7493b2a32e2ba358d6
SenciZ <zenkovic88@gmail.com>
2021-10-18 21:24:28 -0500

    Add state to inputs

--- src/components/NewExpenses/ExpenseForm.js
+++ src/components/NewExpenses/ExpenseForm.js
@@ -1,24 +1,59 @@
+import { useState } from "react";
+
+import Expenses from "../Expenses/Expenses";
 import "./ExpenseForm.css";
 
-function ExpenseForm() {
+function ExpenseForm(props) {
+    const [enteredTitle, setEnteredTitle] = useState("");
+    const [enteredAmount, setEnteredAmount] = useState("");
+    const [enteredDate, setEnteredDate] = useState("");
+
+  const titleChangeHandler = function (e) {
+    setEnteredTitle(e.target.value);
+  };
+  const amountChangeHandler = function (e) {
+    setEnteredAmount(e.target.value);
+  };
+  const dateChangeHandler = function (e) {
+    setEnteredDate(e.target.value);
+  };
+
+  let newExpense = {
+    title: enteredTitle,
+    amount: enteredAmount,
+    date: enteredDate,
+  };
+
   return (
     <form>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" />
+          <input type="text" onChange={titleChangeHandler} />
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
-          <input type="number" min="0.01" step="0.01" />
+          <input
+            type="number"
+            min="0.01"
+            step="0.01"
+            onChange={amountChangeHandler}
+          />
         </div>
         <div className="new-expense__control">
           <label>Date</label>
-          <input type="date" min="2019-01-01" max="2022-12-31" />
+          <input
+            type="date"
+            min="2019-01-01"
+            max="2022-12-31"
+            onChange={dateChangeHandler}
+          />
         </div>
       </div>
-      <div className='new-expense__actions'>
-          <button type="submit">Add New Expense</button>
+      <div className="new-expense__actions">
+        <button type="submit" onSubmit={console.log(newExpense)}>
+          Add New Expense
+        </button>
       </div>
     </form>
   );
