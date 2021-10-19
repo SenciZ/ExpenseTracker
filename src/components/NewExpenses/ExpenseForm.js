5039dbcb94ce254a02389b595038b6909ed7376c
SenciZ <zenkovic88@gmail.com>
2021-10-18 19:57:48 -0500

    Create expense form

---
+++ src/components/NewExpenses/ExpenseForm.js
@@ -0,0 +1,11 @@
+import "./ExpenseForm.css";
+
+function ExpenseForm(){
+
+    return(
+
+
+    );
+};
+
+export default ExpenseForm;
---
+++ src/components/NewExpenses/NewExpense.js
@@ -0,0 +1,11 @@
+import './NewExpense.css';
+
+function NewExpense(){
+
+    return(
+
+
+    );
+};
+
+export default NewExpense;
