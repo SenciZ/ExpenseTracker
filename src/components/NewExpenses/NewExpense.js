6d7f84b9ed428a5859afbc9ff9a4343fa5b75441
Senad Zenkovic <zenkovic88@gmail.com>
2021-11-13 14:41:19 -0700

    two way binding and bottom up communication

--- src/components/NewExpenses/NewExpense.js
+++ src/components/NewExpenses/NewExpense.js
@@ -2,9 +2,14 @@ import "./NewExpense.css";
 import ExpenseForm from "./ExpenseForm";
 
 function NewExpense() {
+
+  const saveExpenseDataHandler = (enteredExpenseData) => {
+
+  }
+
   return (
     <div className="new-expense">
-        <ExpenseForm />
+        <ExpenseForm onSaveExpenseData />
     </div>
   );
 }
